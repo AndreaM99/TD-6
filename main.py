d91499ab31054604f9cf06b0047bf70a8395f865
AndreaM99 <andrea.masse@etu.u-paris.fr>
2021-03-09 08:28:08 +0000

    ajout de l'exercice 2 et des exemples

--- main.py
+++ main.py
@@ -11,8 +11,8 @@ puissance(2, b=3)
 
 ### Question 1 ###
 
-def somme(x,y):
-  return (x+y)
+def somme(a,b):
+  return a+b
 
 x = somme(2,3)
 print(x)
@@ -25,3 +25,38 @@ fonct(1,2, c=4) # là ça marche, car on sait où mettre tous les chiffres..
 
 ### Question 2 ###
 
+fonct(a=1,b=2,c=3)
+fonct(2,2,c=3)
+fonct(b=1,a=2,c=3)
+
+print("---------")
+
+valeurs = 1,2
+c = valeurs[0]
+d = valeurs[1]
+
+res = somme(c,d) # en general on donne le meme nom a la variable qu'au parametre.
+#Cela facilite la lecture mais n'est pas obligatoire.
+print(res)
+
+#ou bien directement
+
+c,d = valeurs
+res = somme(c,d)
+print(res)
+
+res = somme(*valeurs)
+print(res)
+
+kw_valeurs = {'a': 0, 'b': 1}
+res = somme(**kw_valeurs)
+print(res)
+
+print("---------")
+
+valeurs = 1,
+kw_valeurs = {'b':1}
+
+res = somme(*valeurs, **kw_valeurs)
+
+print(res)
